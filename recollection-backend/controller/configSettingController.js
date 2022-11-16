ea0d1fd7bf5efea4ab818a43ebb44bebdeec62c3
Khee Houy, Tan <kheefordev@gmail.com>
2022-11-16 14:52:02 +0800

    UPD: Add API endpoint for add, update and delete.

--- recollection-backend/controller/configSettingController.js
+++ recollection-backend/controller/configSettingController.js
@@ -9,8 +9,72 @@ const getAllConfigSettings = async (req, res) => {
         });
 
     res.json(configSettings);
-}
+};
+
+const addConfigSetting = async (req, res) => {
+    if (!req?.body?.name || !req?.body?.type)
+        return res.status(400).json({
+            "message": "Name and type are required."
+        });
+
+    try {
+        const result = await ConfigSetting.create({
+            name: req.body.name,
+            type: req.body.type,
+            createdBy: "Admin",
+            updatedBy: "Admin"
+        });
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const updateConfigSetting = async (req, res) => {
+    if (!req?.body?.id)
+        return res.status(400).json({
+            "message": "ID is required."
+        });
+
+    const configSetting = await ConfigSetting.findOne({
+        _id: req.body.id
+    }).exec();
+
+    if (!configSetting)
+        return res.status(204).json({
+            "message": `No config setting matches ID ${req.body.id}.`
+        });
+
+    if (req.body?.name) configSetting.name = req.body.name;
+    if (req.body?.type) configSetting.type = req.body.type;
+
+    const result = await configSetting.save();
+    res.status(200).json(result);
+};
+
+const deleteConfigSetting = async (req, res) => {
+    if (!req?.body?.id)
+        return res.status(400).json({
+            "message": "ID is required."
+        });
+
+    const configSetting = await ConfigSetting.findOne({
+        _id: req.body.id
+    }).exec();
+
+    if (!configSetting)
+        return res.status(204).json({
+            "message": `No config setting  matches ID ${req.body.id}`
+        });
+
+    const result = await configSetting.deleteOne({ _id: req.body.id });
+    res.status(200).json(result);
+};
 
 module.exports = {
-    getAllConfigSettings
+    getAllConfigSettings,
+    addConfigSetting,
+    updateConfigSetting,
+    deleteConfigSetting
 }
--- recollection-backend/route/api/configSetting.js
+++ recollection-backend/route/api/configSetting.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const configSettingController = require("../../controller/configSettingController");
 
 router.get("/", configSettingController.getAllConfigSettings);
+router.post("/add", configSettingController.addConfigSetting);
+router.put("/update", configSettingController.updateConfigSetting);
+router.delete("/delete", configSettingController.deleteConfigSetting);
 
 module.exports = router;
